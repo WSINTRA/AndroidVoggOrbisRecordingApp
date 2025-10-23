ceca74a1df67f82728ab3bc148da1cf16aaebd7f
WSINTRA <will.simpson85@gmail.com>
2025-10-22 20:11:02 -0400

    Added 3 band EQ with android.media.audiofx.DynamicsProcessing

--- app/src/androidTest/java/com/example/audiorecorder/ui/screens/MainScreenTest.kt
+++ app/src/androidTest/java/com/example/audiorecorder/ui/screens/MainScreenTest.kt
@@ -3,7 +3,6 @@ package com.example.audiorecorder.ui.screens
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTouchInput
